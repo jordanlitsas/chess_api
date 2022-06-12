a34adda33cb059b40cd9d2ffa834dc5fb91cbd9f
Jordan Litsas <jordanlitsas@live.com.au>
2022-06-12 14:39:32 +1000

    get user's expenses

--- src/main/java/com/spendingTracker/spendingItem/SpendingItemController.java
+++ src/main/java/com/spendingTracker/spendingItem/SpendingItemController.java
@@ -2,6 +2,8 @@ package com.spendingTracker.spendingItem;
 import org.springframework.web.bind.annotation.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.util.ArrayList;
 import java.util.List;
 
 @RestController
@@ -14,9 +16,16 @@ public class SpendingItemController {
         this.repository = repository;
     }
 
-    @GetMapping("/spend")
-    List<SpendingItem> all() {
-        return repository.findAll();
+    @GetMapping("/spend/{userId}")
+    List<SpendingItem> all(@PathVariable String userId) {
+        List<SpendingItem> expenses = repository.findAll();
+        List<SpendingItem> returnExpenses = new ArrayList<SpendingItem>();
+        for (int i = 0; i < expenses.size(); i++){
+            if (expenses.get(i).getUserId().equals(userId)){
+                returnExpenses.add(expenses.get(i));
+            }
+        }
+        return returnExpenses;
     }
 
     @PostMapping(path = "/spend", consumes = "application/json")
