f074847930d2d1ccded4f24d5d2378c943543aac
Jordan Litsas <jordanlitsas@live.com.au>
2022-06-13 10:24:18 +1000

    cors mapping for chrome extension

--- src/main/java/com/spendingTracker/SpendingTrackerApplication.java
+++ src/main/java/com/spendingTracker/SpendingTrackerApplication.java
@@ -2,6 +2,9 @@ package com.spendingTracker;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class SpendingTrackerApplication {
@@ -9,5 +12,13 @@ public class SpendingTrackerApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SpendingTrackerApplication.class, args);
 	}
-
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**").allowedOrigins("*");
+			}
+		};
+	}
 }
